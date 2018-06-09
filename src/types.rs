922646892ec806455269f2c619f6dfb10df86f47
Michael Degano <michael@dega.no>
2018-06-09 12:56:53 +1000

    Fixed unsuccessful dengs counting for points

--- src/types.rs
+++ src/types.rs
@@ -21,8 +21,14 @@ pub struct Deng {
 impl Deng {
     pub fn value(&self) -> i32 {
         let mut value = 0;
-        if self.successful && self.users_first_deng { value += 1 }
-        if self.days_first_deng { value += 1 }
+        if self.successful {
+            if self.users_first_deng {
+                value += 1
+            }
+            if self.days_first_deng {
+                value += 1
+            }
+        }
         value
     }
 }
